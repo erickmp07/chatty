638187115fce0d8cad3de91297120c7964571281
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-20 21:07:50 -0300

    Refactors routes to use the SettingController

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,10 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingRepository } from "./repositories/SettingRepository";
+import { SettingController } from "./controllers/SettingController";
 
 const routes = Router();
 
-routes.post("/settings", async (request, response) => {
-    const { username, chat } = request.body;
-    
-    const settingRepository = getCustomRepository(SettingRepository);
+const settingController = new SettingController();
 
-    const setting = settingRepository.create({
-        username,
-        chat
-    });
-
-    await settingRepository.save(setting);
-
-    return response.json(setting);
-});
+routes.post("/settings", settingController.create);
 
 export { routes };
