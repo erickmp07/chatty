d79230aed530dde694cd26deb860caa4b39f79c2
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-21 16:42:59 -0300

    Adds route to create a message

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
+import { MessageController } from "./controllers/MessageController";
 import { SettingController } from "./controllers/SettingController";
 import { UserController } from "./controllers/UserController";
 
 const routes = Router();
 
+const messageController = new MessageController();
 const settingController = new SettingController();
 const userController = new UserController();
 
 routes.post("/settings", settingController.create);
 routes.post("/users", userController.create);
+routes.post("/messages", messageController.create);
 
 export { routes };
