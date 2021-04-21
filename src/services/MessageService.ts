c8afe515b552d2a7b251ef321dcb85ca911d0c57
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-21 16:24:11 -0300

    Creates MessageService

---
+++ src/services/MessageService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository } from "typeorm";
+import { MessageRepository } from "../repositories/MessageRepository";
+
+interface IMessageCreate {
+    admin_id: string;
+    text: string;
+    user_id: string;
+}
+
+class MessageService {
+    async create({ admin_id, text, user_id }: IMessageCreate) {
+        const messageRepository = getCustomRepository(MessageRepository);
+
+        const message = messageRepository.create({
+            admin_id,
+            text,
+            user_id
+        });
+
+        await messageRepository.save(message);
+
+        return message;
+    }
+}
+
+export { MessageService };
