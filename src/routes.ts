7e61e5b37258d9349864b4ef8f098aac82d82cb8
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-21 15:48:09 -0300

    Adds post route to create User

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { SettingController } from "./controllers/SettingController";
+import { UserController } from "./controllers/UserController";
 
 const routes = Router();
 
 const settingController = new SettingController();
+const userController = new UserController();
 
 routes.post("/settings", settingController.create);
+routes.post("/users", userController.create);
 
 export { routes };
