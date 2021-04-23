e972db70db75eb2a01fd1cb0764975a55c8c5473
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-23 11:07:47 -0300

    Adds route to get the support chat

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,10 @@ app.set("views", publicAssetsPath);
 app.engine("html", renderFile);
 app.set("view engine", "html");
 
+app.get("/pages/client", (request, response) => {
+    return response.render("html/client.html");
+});
+
 const http = createServer(app);
 const io = new Server(http);
 
