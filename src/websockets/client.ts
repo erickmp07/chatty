e6b7bbe4e80bb4f37f54e8183345f7756abd3593
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-23 18:50:27 -0300

    Creates IParams interface

--- src/websockets/client.ts
+++ src/websockets/client.ts
@@ -3,6 +3,11 @@ import { ConnectionService } from "../services/ConnectionService";
 import { MessageService } from "../services/MessageService";
 import { UserService } from "../services/UserService";
 
+interface IParams {
+    text: string;
+    email: string;
+}
+
 io.on("connect", (socket) => {
     const connectionService = new ConnectionService();
     const messageService = new MessageService();
@@ -10,7 +15,7 @@ io.on("connect", (socket) => {
 
     socket.on("client_first_access", async (params) => {
         const socket_id = socket.id;
-        const { text, email } = params;
+        const { text, email } = params as IParams;
         let user_id = null;
 
         const userExists = await userService.findByEmail(email);
