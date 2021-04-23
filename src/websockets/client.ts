289408c7788b067205a042e8439a444c41aba493
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-23 18:47:42 -0300

    Saves the User Message

--- src/websockets/client.ts
+++ src/websockets/client.ts
@@ -1,14 +1,17 @@
 import { io } from "../http";
 import { ConnectionService } from "../services/ConnectionService";
+import { MessageService } from "../services/MessageService";
 import { UserService } from "../services/UserService";
 
 io.on("connect", (socket) => {
     const connectionService = new ConnectionService();
+    const messageService = new MessageService();
     const userService = new UserService();
 
     socket.on("client_first_access", async (params) => {
         const socket_id = socket.id;
         const { text, email } = params;
+        let user_id = null;
 
         const userExists = await userService.findByEmail(email);
 
@@ -19,6 +22,8 @@ io.on("connect", (socket) => {
                 socket_id,
                 user_id: user.id
             });
+
+            user_id = user.id;
         } else {
             const connection = await connectionService.findByUserId(userExists.id);
 
@@ -32,6 +37,13 @@ io.on("connect", (socket) => {
 
                 await connectionService.create(connection);
             }
+
+            user_id = userExists.id;
         }
+
+        await messageService.create({
+            text,
+            user_id
+        })
     });
 });
