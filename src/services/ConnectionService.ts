e6bb0b295f93fd3530c7f3a29f13e7391182cd81
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-23 17:34:51 -0300

    Creates ConnectionService

---
+++ src/services/ConnectionService.ts
@@ -0,0 +1,33 @@
+import { Connection } from "../entities/Connection";
+import { ConnectionRepository } from "../repositories/ConnectionRepository";
+import { getCustomRepository, Repository } from "typeorm";
+
+interface IConnectionCreate {
+    socket_id: string;
+    user_id: string;
+    admin_id: string;
+    id: string;
+}
+
+class ConnectionService {
+    private connectionRepository: Repository<Connection>;
+
+    constructor() {
+        this.connectionRepository = getCustomRepository(ConnectionRepository);
+    }
+
+    async create({ socket_id, user_id, admin_id, id }: IConnectionCreate) {
+        const connection = this.connectionRepository.create({
+            socket_id,
+            user_id,
+            admin_id,
+            id
+        });
+
+        await this.connectionRepository.save(connection);
+
+        return connection;
+    }
+}
+
+export { ConnectionService };
