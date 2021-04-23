6180c8539d8ef63e95d56042c44126f9ccb0984b
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-23 09:06:53 -0300

    Sets the public assets path and the view engine

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
+import path from "path";
 import express from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import { renderFile } from "ejs";
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
 
+const publicAssetsPath = path.join(__dirname, "..", "public");
+
+app.use(express.static(publicAssetsPath));
+app.set("views", publicAssetsPath);
+app.engine("html", renderFile);
+app.set("view engine", "html");
+
 const http = createServer(app);
 const io = new Server(http);
 
 io.on("connection", (socket: Socket) => {
+    console.log("WebSocket connected", socket.id);
 
 });
 
