fb9c72f14325491b1a82e3efcbe8ce078626f0ed
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-23 08:52:14 -0300

    Creates HTTP and WebSocket protocols

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
 
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+
+});
+
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => console.log("Server is running on port 3333."));
+http.listen(3333, () => console.log("Server is running on port 3333."));
