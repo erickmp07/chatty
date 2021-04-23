b6f7d5840ee308754f053ec70d00647a5c852eec
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-23 18:13:47 -0300

    Creates the Connection entity to save

--- src/websockets/client.ts
+++ src/websockets/client.ts
@@ -1,7 +1,29 @@
 import { io } from "../http";
+import { ConnectionService } from "../services/ConnectionService";
+import { UserService } from "../services/UserService";
 
 io.on("connect", (socket) => {
-    socket.on("client_first_access", params => {
-        console.log(params);
+    const connectionService = new ConnectionService();
+    const userService = new UserService();
+
+    socket.on("client_first_access", async (params) => {
+        const socket_id = socket.id;
+        const { text, email } = params;
+
+        const userExists = await userService.findByEmail(email);
+
+        if (!userExists) {
+            const user = await userService.create(email);
+
+            await connectionService.create({
+                socket_id,
+                user_id: user.id
+            });
+        } else {
+            await connectionService.create({
+                socket_id,
+                user_id: userExists.id
+            });
+        }
     });
 });
