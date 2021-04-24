d1d408d28a9b1638898bea90f9d6c7ba6cdd088a
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-24 01:12:06 -0300

    Creates event listener for when User sends Message

--- public/js/chat.js
+++ public/js/chat.js
@@ -62,3 +62,14 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
         document.getElementById("messages").innerHTML += rendered;
     });
 });
+
+document.querySelector("#send_message_button").addEventListener("click", (event) => {
+    const text = document.getElementById("message_user");
+
+    const params = {
+        text,
+        socket_admin_id
+    };
+
+    socket.emit("client_send_to_admin", params);
+});
