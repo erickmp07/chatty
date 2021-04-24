a9c868d491b7ad2dde684a60b8fe0bd90cc54705
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-04-24 00:06:59 -0300

    Lists Messages per User in the admin page

--- src/websockets/admin.ts
+++ src/websockets/admin.ts
@@ -1,10 +1,20 @@
 import { io } from "../http";
 import { ConnectionService } from "../services/ConnectionService";
+import { MessageService } from "../services/MessageService";
 
 io.on("connect", async (socket) => {
     const connectionService = new ConnectionService();
+    const messageService = new MessageService();
 
     const allConnectionsWithoutAdmin = await connectionService.findAllWithoutAdmin();
 
     io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
+
+    socket.on("admin_list_messages_by_user", async (params, callback) => {
+        const { user_id } = params;
+
+        const allMessages = await messageService.listByUser(user_id);
+
+        callback(allMessages);
+    });
 });
